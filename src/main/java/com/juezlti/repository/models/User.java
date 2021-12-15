ca0ef5abb06767e287afc89a67ec52b41c042577
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2021-12-15 09:42:04 +0100

    Hide password for reading

--- src/main/java/com/juezlti/repository/models/User.java
+++ src/main/java/com/juezlti/repository/models/User.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -13,25 +14,23 @@ import lombok.Data;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "user")
 public class User {
-    
+
     @Id
 	private String id;
-	
+
     @JsonProperty("displayname")
     private String displayName;
-    
-    @JsonProperty("username")
-    private String userName;
-    
-    private String email;
-    
-    private String password;
 
-    public User(String userName, String email, String password) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-    }
+    @JsonProperty("username")
+    @Indexed(unique=true)
+    private String userName;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty( value = "password", access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
